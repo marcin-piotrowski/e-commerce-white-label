6f5e52a1dc16c1d93304b2fe81fe13e51744c7be
Marcin Piotrowski <piotrowskimarcin@hotmail.com>
2021-10-28 00:56:11 +0200

    got it

--- assets/js/ephesus.js
+++ assets/js/ephesus.js
@@ -1,5 +1,4 @@
 function toggleNightMode(){
 	document.documentElement.setAttribute('data-theme', 'light');
-	document.getElementById('mode-switcher').classList.remove('active');
 	localStorage.setItem("theme","");
 }
