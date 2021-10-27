fcb8737bacee26399eeda445baceafda8d0e1f06
Marcin Piotrowski <piotrowskimarcin@hotmail.com>
2021-10-28 00:16:45 +0200

    fix3

--- _site/assets/js/ephesus.js
+++
@@ -1,5 +0,0 @@
-function toggleNightMode(){
-	document.documentElement.setAttribute('data-theme', 'light');
-	document.getElementById('mode-switcher').classList.remove('active');
-	localStorage.setItem("theme","");
-}
